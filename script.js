b98d4aa418e42f53a8a412c6e657cf6fdeadd771
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-02-19 16:35:31 +0530

    commit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
 document.addEventListener("DOMContentLoaded" , ()=>{
     let abc=document.querySelector(".Projects-Container")
     let fixed = document.querySelector(".fixed-videos")
@@ -41,5 +43,41 @@ document.addEventListener("DOMContentLoaded" , ()=>{
     project6.addEventListener("mouseenter", ()=>{
         let img = project6.getAttribute("data-image")
         fixed.style.backgroundImage = `url(${img})`
+    })
+
+    // We can also use forEach for better optimization
+    let rightimg = document.querySelector('.rightside')
+    let prj1 = document.querySelector('#h101')
+    let prj2 = document.querySelector('#h102')
+    let prj3 = document.querySelector('#h103')
+    prj1.addEventListener('click', ()=>{
+        let para1 = document.querySelector('.leftside p')
+        para1.innerHTML="Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings."
+
+        prj1.style.color = "#EFEAE3"
+        prj2.style.color = "#504A45"
+        prj3.style.color = "#504A45"
+       
+        rightimg.style.backgroundImage ='url(https://assets-global.website-files.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15e1_Project-p-1080.jpg)'
+       
+    })
+    prj2.addEventListener('click', ()=>{
+        let para2 = document.querySelector('.leftside p')
+        para2.innerHTML="Once we have a design, our production team<br> takes the lead in bringing it to life. We manage all<br> stages of the project, from build specifications and<br> technical drawings to site surveys, vendor<br> management, and 2D & 3D production. We have<br> an extensive network of partners to meet each<br> unique design and project need."
+        prj2.style.color = "#EFEAE3"
+        prj1.style.color = "#504A45"
+        prj3.style.color = "#504A45"
+        
+       rightimg.style.backgroundImage ='url(https://assets-global.website-files.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15d0_Project.webp)'
+       
+    })
+    prj3.addEventListener('click', ()=>{
+        let para = document.querySelector('.leftside p')
+        para.innerHTML="We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience."
+        prj3.style.color = "#EFEAE3"
+        prj1.style.color = "#504A45"
+        prj2.style.color = "#504A45"
+        rightimg.style.backgroundImage ='url(https://cdn.dribbble.com/users/1121893/screenshots/5555705/dribbble-eline_ye.gif)'
+       
     })
 })
