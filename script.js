57ed7510b3e1042a158bd43442472291d2c8a95d
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-02-19 12:26:47 +0530

    commit

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+document.addEventListener("DOMContentLoaded" , ()=>{
+    let abc=document.querySelector(".Projects-Container")
+    let fixed = document.querySelector(".fixed-videos")
+    abc.addEventListener('mouseenter', ()=>{
+        fixed.style.display = "block"
+    })
+    abc.addEventListener("mouseleave", ()=>{
+        fixed.style.display = "none"
+    })
+})
