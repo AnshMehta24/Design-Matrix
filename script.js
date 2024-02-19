f89ce7307615df06a8927ca8a2fa6872ea50025e
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-02-19 13:34:37 +0530

    commit

--- script.js
+++ script.js
@@ -11,4 +11,35 @@ document.addEventListener("DOMContentLoaded" , ()=>{
     abc.addEventListener("mouseleave", ()=>{
         fixed.style.display = "none"
     })
+
+    let project1 = document.querySelector("#project1")
+    let project2 = document.querySelector("#project2")
+    let project3 = document.querySelector("#project3")
+    let project4 = document.querySelector("#project4")
+    let project5 = document.querySelector("#project5")
+    let project6 = document.querySelector("#project6")
+    project1.addEventListener("mouseenter", ()=>{
+        let img = project1.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
+    project2.addEventListener("mouseenter", ()=>{
+        let img = project2.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
+    project3.addEventListener("mouseenter", ()=>{
+        let img = project3.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
+    project4.addEventListener("mouseenter", ()=>{
+        let img = project4.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
+    project5.addEventListener("mouseenter", ()=>{
+        let img = project5.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
+    project6.addEventListener("mouseenter", ()=>{
+        let img = project6.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
 })
